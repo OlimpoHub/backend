8e28d7da49869a5e8e9d7b0f2d49fb278b4a9b59
Ale Arredondo <alejandrarredondo09@gmail.com>
2025-10-31 08:06:00 -0600

    [FIX] connection with .env

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,7 @@
 import mariadb from "mariadb";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: "../.env" });
 
 export const pool = mariadb.createPool({
   host: process.env.DB_HOST,
@@ -11,3 +11,15 @@ export const pool = mariadb.createPool({
   port: process.env.DB_PORT,
   connectionLimit: 10,
 });
+
+
+// USE THIS TO TEST THE CONNECTION WITH THE DATABASE AND SEE THE ERRORS IF THERE ARE ANY
+// try {
+//   const conn = await pool.getConnection();
+//   console.log("✅ Conexión exitosa a la base de datos");
+//   conn.release();
+// } catch (err) {
+//   console.error("❌ Error al conectar a la base de datos:", err);
+// } finally {
+//   process.exit();
+// }
