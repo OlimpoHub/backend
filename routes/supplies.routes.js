eb3cbf05ae1fe47849367fa227fd4d41f3313c1e
Luisa Fernanda Valdez Guillen <A01711870@tec.mx>
2025-11-02 12:51:17 -0600

    [UPDATE] Changelog and comments on the code

--- routes/supplies.routes.js
+++ routes/supplies.routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const suppliesController = require("../controllers/supplies.controller");
 
+// Define a GET endpoint to retrieve all supplies
 router.get("/", suppliesController.getSupplies);
 
+// Export the router to be used in the main routes file
 module.exports = router;
