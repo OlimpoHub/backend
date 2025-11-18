6060754e1867c071e92c78c679abf74f37ebe53a
23456312 <A01707346@tec.mx>
2025-11-17 19:13:05 -0600

    [FIX] Product models

--- utils/encryption.js
+++ utils/encryption.js
@@ -1,21 +1,12 @@
 const crypto = require("crypto");
 const ALGORITHM = "aes-256-cbc";
+const SECRET_KEY = crypto.createHash("sha256").update(process.env.QR_SECRET_KEY).digest();
 const IV_LENGTH = 16;
-
-// Helper function to get the secret key with validation
-function getSecretKey() {
-    const secretKey = process.env.QR_SECRET_KEY;
-    if (!secretKey) {
-        throw new Error("QR_SECRET_KEY environment variable is not set");
-    }
-    return crypto.createHash("sha256").update(secretKey).digest();
-}
 
 // Encrypts a text based on a key
 exports.encrypt = (text) => {
-    const secretKey = getSecretKey();
     const iv = crypto.randomBytes(IV_LENGTH);
-    const cipher = crypto.createCipheriv(ALGORITHM, secretKey, iv);
+    const cipher = crypto.createCipheriv(ALGORITHM, SECRET_KEY, iv);
     let encrypted = cipher.update(text, "utf8", "base64");
     encrypted += cipher.final("base64");
     return iv.toString("base64") + ":" + encrypted;
@@ -23,10 +14,9 @@ exports.encrypt = (text) => {
 
 // Decrypts a text based on a key
 exports.decrypt = (encryptedText) => {
-    const secretKey = getSecretKey();
     const [ivBase64, encryptedData] = encryptedText.split(":");
     const iv = Buffer.from(ivBase64, "base64");
-    const decipher = crypto.createDecipheriv(ALGORITHM, secretKey, iv);
+    const decipher = crypto.createDecipheriv(ALGORITHM, SECRET_KEY, iv);
     let decrypted = decipher.update(encryptedData, "base64", "utf8");
     decrypted += decipher.final("utf8");
     return decrypted;
